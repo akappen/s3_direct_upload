2fceaa53226a8c36904e25dffcfde12363ab807b
Andrew Kappen <akappen@gmail.com>
2017-02-17 10:33:27 -0600

    New elixir project for S3 direct uploads

---
+++ lib/s3_direct_upload.ex
@@ -0,0 +1,18 @@
+defmodule S3DirectUpload do
+  @moduledoc """
+  Documentation for S3DirectUpload.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> S3DirectUpload.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/s3_direct_upload_test.exs
@@ -0,0 +1,8 @@
+defmodule S3DirectUploadTest do
+  use ExUnit.Case
+  doctest S3DirectUpload
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
